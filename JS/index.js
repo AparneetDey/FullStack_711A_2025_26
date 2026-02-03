623a3265e8ea115190cf0c48338652c00fec3e74
Aparneet Dey <aparnitd@gmail.com>
2026-02-03 14:47:13 +0530

    Add Todo List application with HTML, CSS, and JavaScript. Implemented basic functionality for adding and deleting todos, along with a responsive layout and styling for improved user experience.

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+// 
+
+// let num;
+// console.log(num);
+
+// var num1 = 10;
+// var num1 = 20;
+
+// console.log(num1);
+
+const func = () => {}
+
+console.log(typeof(NaN));
---
+++ Todo-list/index.js
@@ -0,0 +1,55 @@
+// Get DOM elements
+const todoInput = document.querySelector('#todo-input');
+const addBtn = document.querySelector('#add-btn');
+const todoList = document.querySelector('#todo-list');
+
+// Load todos from localStorage
+let todos = [];
+
+// Function to render todos
+function renderTodos() {
+    if (todos.length === 0) {
+        todoList.innerHTML = '<li class="empty-message">No todos yet. Add one above!</li>';
+        return;
+    }
+
+    todoList.innerHTML = todos.map((todo, index) => `
+        <li>
+            <span class="todo-text">${todo}</span>
+            <button class="delete-btn" onclick="deleteTodo(${index})">Delete</button>
+        </li>
+    `).join('');
+}
+
+// Function to add a new todo
+function addTodo() {
+    const todoText = todoInput.value.trim();
+
+    if (todoText === '') {
+        alert('Please enter a todo item!');
+        return;
+    }
+
+    todos.push(todoText);
+    todoInput.value = '';
+    renderTodos();
+}
+
+// Function to delete a todo
+function deleteTodo(index) {
+    if (confirm('Are you sure you want to delete this todo?')) {
+        todos.splice(index, 1);
+        renderTodos();
+    }
+}
+
+// Event listeners
+addBtn.addEventListener('click', addTodo);
+todoInput.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        addTodo();
+    }
+});
+
+// Initial render
+renderTodos();
